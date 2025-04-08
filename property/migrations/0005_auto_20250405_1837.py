65febac903fe69e7d15aa60fd61811fb1fb4dcbc
Alex <1kimalexander@gmail.com>
2025-04-08 19:58:15 +0400

    Change year from 2005 to 2015.

--- property/migrations/0005_auto_20250405_1837.py
+++ property/migrations/0005_auto_20250405_1837.py
@@ -5,8 +5,8 @@ from django.db import migrations
 
 def fill_new_building(apps, schema_editor):
     Flat = apps.get_model('property','Flat')
-    Flat.objects.filter(construction_year__gte=2005).update(new_building=True)
-    Flat.objects.filter(construction_year__lt=2005).update(new_building=False)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 
